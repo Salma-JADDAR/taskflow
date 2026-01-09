0f391b78060b8cc268bc9e17bc0d8536cffac5b1
Salma GTU <salmajaddar7@gmail.com>
2026-01-09 11:51:31 +0100

    la connexion avec la base de données

---
+++ app/Core/connetion.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+namespace App\Core;
+
+use PDO;
+
+class Database{
+    private static ?PDO $instance = null;
+
+    private function __construct() {}
+    private function __clone() {}
+    private function __wakeup() {}
+
+    public static function getInstance(): PDO{
+        if (Database::$instance === null) {
+            try{
+            Database::$instance = new PDO(
+                "mysql:host=localhost;dbname=taskflow;charset=utf8","root",""
+            );
+            }catch (PDOException $e) {
+                die("erreur:". $e->getMessage());
+            }
+        }
+        return Database::$instance;
+        
+    }
+}
